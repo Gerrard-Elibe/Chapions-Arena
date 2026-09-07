f15066703e651db8badfa14ea18c64c0a84fa7f2
Gerrard-Elibe <gerardelibe@gmail.com>
2026-09-07 09:56:50 +0100

    new

--- src/Data/fixtures.js
+++ src/Data/fixtures.js
@@ -1,107 +1,107 @@
 export const fixtures = [
 // =========================
-  // MATCHDAY 18 (UPCOMING)
-  // =========================
+// MATCHDAY 18 (UPCOMING)
+// =========================
 
-  {
-    id: 103,
-    matchday: 18,
-    day: "Monday",
-    player1: "Samson",
-    player2: "Emmanuel",
-    status: "Upcoming",
-  },
-  {
-    id: 104,
-    matchday: 18,
-    day: "Monday",
-    player1: "Patrick",
-    player2: "Eman",
-    status: "Upcoming",
-  },
-  {
-    id: 105,
-    matchday: 18,
-    day: "Monday",
-    player1: "Sixtus",
-    player2: "Gerrard",
-    status: "Upcoming",
-  },
-  {
-    id: 106,
-    matchday: 18,
-    day: "Tuesday",
-    player1: "Mr Chaos",
-    player2: "Bright",
-    status: "Upcoming",
-  },
-  {
-    id: 107,
-    matchday: 18,
-    day: "Tuesday",
-    player1: "Stoner",
-    player2: "Offorzor",
-    status: "Upcoming",
-  },
-  {
-    id: 108,
-    matchday: 18,
-    day: "Tuesday",
-    player1: "Louis",
-    player2: "Ezenwa",
-    status: "Upcoming",
-  },
+{
+  id: 103,
+  matchday: 18,
+  day: "Monday",
+  player1: "Samson",
+  player2: "Emmanuel",
+  status: "Upcoming",
+},
+{
+  id: 104,
+  matchday: 18,
+  day: "Monday",
+  player1: "Patrick",
+  player2: "Eman",
+  status: "Upcoming",
+},
+{
+  id: 105,
+  matchday: 18,
+  day: "Monday",
+  player1: "Sixtus",
+  player2: "Gerrard",
+  status: "Upcoming",
+},
+{
+  id: 106,
+  matchday: 18,
+  day: "Tuesday",
+  player1: "Mr Chaos",
+  player2: "Bright",
+  status: "Upcoming",
+},
+{
+  id: 107,
+  matchday: 18,
+  day: "Tuesday",
+  player1: "Stoner",
+  player2: "Offorzor",
+  status: "Upcoming",
+},
+{
+  id: 108,
+  matchday: 18,
+  day: "Tuesday",
+  player1: "Louis",
+  player2: "Ezenwa",
+  status: "Upcoming",
+},
 
-  // =========================
-  // MATCHDAY 19 (UPCOMING)
-  // =========================
+// =========================
+// MATCHDAY 19 (UPCOMING)
+// =========================
 
-  {
-    id: 109,
-    matchday: 19,
-    day: "Thursday",
-    player1: "Sixtus",
-    player2: "Patrick",
-    status: "Upcoming",
-  },
-  {
-    id: 110,
-    matchday: 19,
-    day: "Thursday",
-    player1: "Offorzor",
-    player2: "Samson",
-    status: "Upcoming",
-  },
-  {
-    id: 111,
-    matchday: 19,
-    day: "Thursday",
-    player1: "Emmanuel",
-    player2: "Ezenwa",
-    status: "Upcoming",
-  },
-  {
-    id: 112,
-    matchday: 19,
-    day: "Friday",
-    player1: "Eman",
-    player2: "Bright",
-    status: "Upcoming",
-  },
-  {
-    id: 113,
-    matchday: 19,
-    day: "Friday",
-    player1: "Louis",
-    player2: "Gerrard",
-    status: "Upcoming",
-  },
-  {
-    id: 114,
-    matchday: 19,
-    day: "Friday",
-    player1: "Mr Chaos",
-    player2: "Stoner",
-    status: "Upcoming",
-  },
+{
+  id: 109,
+  matchday: 19,
+  day: "Thursday",
+  player1: "Sixtus",
+  player2: "Patrick",
+  status: "Upcoming",
+},
+{
+  id: 110,
+  matchday: 19,
+  day: "Thursday",
+  player1: "Offorzor",
+  player2: "Samson",
+  status: "Upcoming",
+},
+{
+  id: 111,
+  matchday: 19,
+  day: "Thursday",
+  player1: "Emmanuel",
+  player2: "Ezenwa",
+  status: "Upcoming",
+},
+{
+  id: 112,
+  matchday: 19,
+  day: "Friday",
+  player1: "Eman",
+  player2: "Bright",
+  status: "Upcoming",
+},
+{
+  id: 113,
+  matchday: 19,
+  day: "Friday",
+  player1: "Louis",
+  player2: "Gerrard",
+  status: "Upcoming",
+},
+{
+  id: 114,
+  matchday: 19,
+  day: "Friday",
+  player1: "Mr Chaos",
+  player2: "Stoner",
+  status: "Upcoming",
+},
 ];
